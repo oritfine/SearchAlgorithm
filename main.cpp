242589ba590b0db686a4a0f570d2d251fabd4187
Yana Molodetsky <59420437+yanamolo@users.noreply.github.com>
2020-01-26 13:32:28 +0200

    main updated

--- main.cpp
+++ main.cpp
@@ -2,21 +2,25 @@
 #include "ClientHandler.h"
 #include "MyClientHandler.h"
 #include "Server.h"
-#include "MySerialServer.h"
 #include "FileCacheManager.h"
 #include "Searchable.h"
-#include "BestFS.h"
+#include "Astar.h"
 #include "MyParallelServer.h"
 
 #define NUM_CLIENTS 15
 
-int main(/*int argc, char* argv[]*/) {
-    AbstSearcher<Searchable<State<Point*>*>*, Solution<State<Point*>*>*, State<Point*>*> *search = new BestFS();
+int main(int argc, char* argv[]) {
+    AbstSearcher<Searchable<State<Point*>*>*, Solution<State<Point*>*>*, State<Point*>*> *search = new Astar();
     Solver<Searchable<State<Point*>*>*, Solution<State<Point*>*>*> *solver = new OA<Searchable<State<Point*>*>*, Solution<State<Point*>*>*, State<Point*>*>(search);
     CacheManager<string , string> *cm = new FileCacheManager<string>();
     ClientHandler<Searchable<State<Point*>*> *, Solution<State<Point*>*>*, string, string>* c = new MyClientHandler(solver, cm);
     server_side::Server<Searchable<State<Point*>*>*, Solution<State<Point*>*>*, string, string> *server =  new MyParallelServer();
-    //server->open(atoi(argv[0]), c);
-    server->open(NUM_CLIENTS, 5700, c);
+    if (argv[0] != NULL) {
+        server->open(NUM_CLIENTS,atoi(argv[0]), c);
+    }
+    else {
+        server->open(NUM_CLIENTS, 5700, c);
+    }
+
     return 0;
 };
